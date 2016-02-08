f75ac65916afbd7e6844a1f48e237a35c686cc33
Maksim Stecenko <max.stecenko.max@gmail.com>
2016-02-08 23:20:35 +0200

    bug fixed

--- PartyMaker/PMRAddEventViewController.m
+++ PartyMaker/PMRAddEventViewController.m
@@ -14,7 +14,35 @@
 @interface PMRAddEventViewController ()<UITextFieldDelegate,
                                         UIScrollViewDelegate,
                                         UITextViewDelegate>
+
 @property (nonatomic) NSMutableString* lastTextViewEditText;
+
+@property (nonatomic, weak) IBOutlet UIButton *chooseDateButton;
+
+@property (nonatomic, weak) IBOutlet UIButton *saveButton;
+@property (nonatomic, weak) IBOutlet UIButton *cancelButton;
+
+@property (nonatomic, weak) IBOutlet UITextField *eventNameTextField;
+
+@property (nonatomic, weak) IBOutlet UIView *dynamicBall;
+@property (nonatomic, weak) IBOutlet UIView *datePickerView;
+
+@property (nonatomic, weak) IBOutlet UILabel *startTimeLabel;
+@property (nonatomic, weak) IBOutlet UILabel *endTimeLabel;
+
+@property (nonatomic, weak) IBOutlet UISlider *startTimeSlider;
+@property (nonatomic, weak) IBOutlet UISlider *endTimeSlider;
+
+@property (nonatomic, weak) IBOutlet UIScrollView *imageScrollView;
+@property (nonatomic, weak) IBOutlet UIPageControl *imagePageControl;
+
+@property (nonatomic, weak) IBOutlet UITextView *descriptionTextView;
+
+@property (nonatomic, weak) IBOutlet UIDatePicker *datePickerControl;
+
+@property (nonatomic, weak) IBOutlet UIToolbar *datePickerToolBar;
+@property (nonatomic, weak) IBOutlet UIToolbar *keyboardToolBar;
+
 @end
 
 @implementation PMRAddEventViewController
